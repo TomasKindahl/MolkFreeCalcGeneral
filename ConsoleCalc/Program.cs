dad8f0ec32abc6f1eb31b36e9fb57d83d3aadb82
Alejandra lopez pineda <al.lopezpineda@gmail.com>
2020-11-17 15:57:34 +0100

    Load file

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,9 +17,55 @@ namespace ConsoleCalc
             Console.WriteLine("welcome to calc");
             string input;
             string[] commands;
-            //string currentFile = @".\kommandoFile.txt";
+            string currentFile = @"C:\Users\allop\Source\Repos\MolkFreeCalcGeneral\ConsoleCalc\kommandoList.txt";
 
+            using (StreamReader fileAddress = new StreamReader(currentFile))
+            {
+                while (fileAddress.Peek() >= 0)
+                {
+                    string inputFile = fileAddress.ReadLine();
+                    commands = inputFile.Split(' ');
 
+                    if (commands[0] == "quit")
+                    {//
+                        Console.WriteLine("hej då");
+                        stop = true;
+                    }
+                    else if (commands[0] == "enter" && commands.Length == 2)
+                    {
+                        cs.entry = commands[1];
+                        cs.Enter();
+
+                    }
+                    else if (commands[0] == "+")
+                    {
+                        cs.BinOp("+");
+                    }
+                    else if (commands[0] == "*")
+                    {
+                        cs.BinOp("×");
+                    }
+                    else if (commands[0] == "-")
+                    {
+                        cs.BinOp("−");
+                    }
+                    else if (commands[0] == "/")
+                    {
+                        cs.BinOp("÷");
+                    }
+                    else if (commands[0] == "show")
+                    {
+                        Console.WriteLine($"T: {cs.T}");
+                        Console.WriteLine($"Z: {cs.Z}");
+                        Console.WriteLine($"Y: {cs.Y}");
+                        Console.WriteLine($"X: {cs.X}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("wrong input, try again");
+                    }
+                }
+            }
 
             do
             {
