b780fa7050da88d6bf0a89aca45ad36dc65dc217
Adnan Kadrispahic <adikadrispahic@gmail.com>
2020-11-17 11:45:26 +0100

    Working on command prompt

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -10,11 +10,42 @@ namespace ConsoleCalc
     class Program
     {
         static void Main(string[] args)
-        {
-            //
+        {           
             CStack cs;
-            cs = new CStack();
-            cs.X = 12;
+            string command;
+            cs = new CStack();          
+            
+            do{
+
+                Console.Write("> ");
+                command = Console.ReadLine();
+                string[] commandWord = command.Split(' ');
+
+                if (command == "show"){
+                    Console.WriteLine("T: "+(cs.T));
+                    Console.WriteLine("Z: "+(cs.Z));
+                    Console.WriteLine("Y: "+(cs.Y));
+                    Console.WriteLine("X: "+(cs.X));
+                }
+                else if (commandWord[0] == "enter")
+	            {
+                    double num;
+                    double.TryParse(commandWord[1], out num);                      
+                    cs.T = cs.Z; 
+                    cs.Z = cs.Y; 
+                    cs.Y = cs.X; 
+                    cs.X = num;
+	            }
+                else if (command == "+")
+	            {
+                                     
+	            }
+	
+	        } while (command != "quit");
+
         }
+
+       
+
     }
 }
