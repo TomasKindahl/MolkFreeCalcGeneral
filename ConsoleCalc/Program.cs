7d589bef9352acefa09edc764cc7a7df09f09303
Rippan <jepperipnas@gmail.com>
2020-11-17 19:58:54 +0100

    1b

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,63 +13,67 @@ namespace ConsoleCalc
     {
         static void Main(string[] args)
         {
+            bool quit = false;
             CStack cs;
             cs = new CStack();
-            bool stop = false;
-            Console.WriteLine("welcome to calc");
             string input;
-            string[] commands;
+            string[] words;
+            Console.WriteLine("ConsoleCalc Grupp 4");
             do
             {
                 Console.Write("> ");
                 input = Console.ReadLine();
-                commands = input.Split(' ');
+                words = input.Split(' ');
+                switch (words[0])
+                {
+                    case "+": cs.BinOp("+"); break;
+                    case "-": cs.BinOp("-"); break;
+                    case "*": cs.BinOp("x"); break;
+                    case "÷": cs.BinOp("÷"); break;
+                    case "quit":
+                        quit = true;
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    case "enter":
+                        cs.entry = words[1]; 
+                        cs.Enter();
+                        break;
+                    case "show":
+                        Console.WriteLine($"X = {cs.X}");
+                        Console.WriteLine($"Y = {cs.Y}");
+                        Console.WriteLine($"Z = {cs.Z}");
+                        Console.WriteLine($"T = {cs.T}");
+                        break;
+                    case "load":
+                        string filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"..\..\..\data\commands.txt");
+                        string[] fileText = File.ReadAllText(filePath).Split(';');
 
-                if ( commands[0] == "quit")
-                {//
-                    Console.WriteLine("hej då");
-                    stop = true;
+                        for (int i = 0; i < fileText.Length; i++)
+                        {
+                            string[] word = fileText[i].Split(' ');
+                            switch (word[0])
+                            {
+                                case "enter":
+                                    cs.entry = word[1];
+                                    cs.Enter();
+                                    break;
+                                    case "+": cs.BinOp("+"); break;
+                                    case "-": cs.BinOp("-"); break;
+                                    case "*": cs.BinOp("x"); break;
+                                    case "÷": cs.BinOp("÷"); break;
+                                case "show":
+                                    Console.WriteLine($"X = {cs.X}");
+                                    Console.WriteLine($"Y = {cs.Y}");
+                                    Console.WriteLine($"Z = {cs.Z}");
+                                    Console.WriteLine($"T = {cs.T}");
+                                    break;
+                                default: break;
+                            }
+                        }
+                        break;
+                    default: break;
                 }
-                else if ( commands[0] == "enter" && commands.Length == 2 )
-                {
-                    cs.entry = commands[1];
-                    cs.Enter();
-                    Console.WriteLine("test");
-
-                }
-                else if (commands[0] == "+")
-                {
-                    cs.BinOp("+");
-                }
-                else if (commands[0] == "*")
-                {
-                    cs.BinOp("×");
-                }
-                else if (commands[0] == "-")
-                {
-                    cs.BinOp("−");
-                }
-                else if (commands[0] == "/")
-                {
-                    cs.BinOp("÷");
-                }
-                else if (commands[0] == "show")
-                {
-                    Console.WriteLine($"T: {cs.T.ToString()}");
-                    Console.WriteLine($"Z: {cs.Z.ToString()}");
-                    Console.WriteLine($"Y: {cs.Y.ToString()}");
-                    Console.WriteLine($"X: {cs.X.ToString()}");
-                }
-                else
-                {
-                    Console.WriteLine("wrong input, try again");
-                }
-
-            } while (!stop);
-
-
-            Console.ReadKey();
-
+            } while (!quit);
         }
     }
 }
