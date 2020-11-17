a2c21d47bf14e5c820e5bea6ac03202c2696694f
yungdeni <asertyx@gmail.com>
2020-11-17 11:57:04 +0100

    Fixat

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using CStackClass;
 
 namespace ConsoleCalc
@@ -16,22 +13,50 @@ namespace ConsoleCalc
             string command = "";
             string[] commandWord;
             while (command != "quit")
-	        {
+            {
                 command = Console.ReadLine();
                 commandWord = command.Split(' ');
-                if (command.Length == 1)
-                {
-                    cs.BinOp(command);
-                }
-                else if (commandWord[0] == "enter")
-                {
-                    cs.RollSetX(double.Parse(commandWord[1]));
-                }
-                else if(command == "show")
-                {
-                    Console.WriteLine(cs.StackString());
-                }
+                ParseInput(cs, command, commandWord);
 
+            }
+        }
+
+        private static void ParseInput(CStack cs, string command, string[] commandWord)
+        {
+            if (command.Length == 1)
+            {
+                cs.BinOp(command);
+            }
+            else if (commandWord[0] == "enter")
+            {
+                cs.RollSetX(double.Parse(commandWord[1]));
+            }
+            else if (command == "show")
+            {
+                Console.WriteLine(cs.StackString());
+            }
+            else if (commandWord[0] == "load")
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader("TestFile.txt"))
+                    {
+                        string line;
+
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            string[] lineWord = line.Split(' ');
+                            ParseInput(cs, line, lineWord);
+
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    // Let the user know what went wrong.
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(e.Message);
+                }
             }
         }
     }
