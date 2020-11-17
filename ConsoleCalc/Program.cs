470d144a758ad98ee1d9c05bfa8f9e9877783460
Karl Eriksson <iuff12345@gmail.com>
2020-11-17 12:07:19 +0100

    1.b

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CStackClass;
+using System.IO;
 
 namespace ConsoleCalc
 {
@@ -37,8 +38,37 @@ namespace ConsoleCalc
                     case "/":
                         cs.BinOp(commandWord[0]);
                         break;
-
-
+                    case "load":
+                        string filepath = commandWord[1];
+                        string[] tmp = File.ReadAllLines(filepath);
+                        for(int i = 0; i < tmp.Length; i++)
+                        {
+                            commandWord = tmp[i].Split(' ');
+                            switch (commandWord[0]){
+                                case "enter":
+                                    double newXLoad = double.Parse(commandWord[1]);
+                                    cs.RollSetX(newXLoad);
+                                    break;
+                                case "+":
+                                    cs.BinOp(commandWord[0]);
+                                    break;
+                                case "-":
+                                    cs.BinOp(commandWord[0]);
+                                    break;
+                                case "*":
+                                    cs.BinOp(commandWord[0]);
+                                    break;
+                                case "/":
+                                    cs.BinOp(commandWord[0]);
+                                    break;
+                                case "show":
+                                    Console.WriteLine(cs.StackString());
+                                    break;
+                                case "quit":
+                                    check = true;
+                                    break;
+                            }
+                        }
                         break;
                     case "show":
                         Console.WriteLine(cs.StackString());
