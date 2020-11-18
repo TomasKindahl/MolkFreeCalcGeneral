4c7e27a19684acd34d3be79bb3537d19decc285f
Samuel Karlström <sam.karlstrm@gmail.com>
2020-11-18 10:53:16 +0100

    test123

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -1,9 +1,7 @@
-﻿ using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;   
-using System.Threading.Tasks;
+﻿using System;
+using System.IO;
 using CStackClass;
+using System.Collections.Generic;
 
 namespace ConsoleCalc
 {
@@ -11,29 +9,53 @@ namespace ConsoleCalc
     {
         static void Main(string[] args)
         {
+
             CStack cs;
             cs = new CStack();
             bool stop = false;
-            Console.WriteLine("welcome to calc");
             string input;
-            string[] commands;
-            do
-            {
-                Console.Write("> ");
-                input = Console.ReadLine();
-                commands = input.Split(' ');
+            string[] commands = { };
+            List<string> lineList = new List<string>();
 
-                if ( commands[0] == "quit")
-                {//
+            Console.WriteLine("welcome to calc");
+            while (!stop)
+            {
+                if (lineList.Count == 0)
+                {
+                    Console.Write("> ");
+                    input = Console.ReadLine();
+                    commands = input.Split(' ');
+                }
+                if (lineList.Count > 0)
+                {
+                    commands = lineList[0].Split(' ');
+                    //Console.WriteLine(commands[0]);
+                    lineList.RemoveAt(0);
+                }
+
+                if (commands[0] == "quit" )
+                {
                     Console.WriteLine("hej då");
                     stop = true;
                 }
-                else if ( commands[0] == "enter" && commands.Length == 2 )
+                else if (commands[0] == "enter" && commands.Length == 2)
                 {
                     cs.entry = commands[1];
                     cs.Enter();
-                    Console.WriteLine("test");
 
+                }
+                else if (commands[0] == "load")
+                {
+                    string file_name = @"C:\Users\samka\progmet\freecalc.lis";
+
+                    if (File.Exists(file_name))
+                    {
+                        string[] stringLines = File.ReadAllLines(file_name);
+                        foreach (string line in stringLines)
+                        {
+                            lineList.Add(line);
+                        }
+                    }
                 }
                 else if (commands[0] == "+")
                 {
@@ -53,23 +75,16 @@ namespace ConsoleCalc
                 }
                 else if (commands[0] == "show")
                 {
-                    Console.WriteLine($"T: {cs.T.ToString()}");
-                    Console.WriteLine($"Z: {cs.Z.ToString()}");
-                    Console.WriteLine($"Y: {cs.Y.ToString()}");
-                    Console.WriteLine($"X: {cs.X.ToString()}");
-                }
-                else
-                if(commands[0]=="clear")
-                {
-                    cs.Reset();
-                    Console.WriteLine("Done...");
+                    Console.WriteLine($"T: {cs.T}");
+                    Console.WriteLine($"Z: {cs.Z}");
+                    Console.WriteLine($"Y: {cs.Y}");
+                    Console.WriteLine($"X: {cs.X}");
                 }
                 else
                 {
                     Console.WriteLine("wrong input, try again");
                 }
-
-            } while (!stop);
+            }
 
 
             Console.ReadKey();
